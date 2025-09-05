b12f4e3bf59cefd17a20966d5b191ef9f2190869
Alfaz Bilakhiya <alfazbilakhiya17@gmail.com>
2025-09-05 15:50:53 +0530

    .

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL:`${import.meta.env.VITE_API_BASE_URL}/api`,
+  baseURL:`https://corona-marine.onrender.com/api`,
   withCredentials: true // Important for cookies
 });
 
