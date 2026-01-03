4c99e28c70d43958769a72b99c0341e6b9a25451
Alfaz Bilakhiya <alfazbilakhiya17@gmail.com>
2026-01-03 13:58:24 +0530

    .

--- backend/config/generateToken.js
+++ backend/config/generateToken.js
@@ -2,6 +2,8 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
 
+
+
 export const generateToken = (userId) => {
   return jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
